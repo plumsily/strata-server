15c779222c6e7d242bf2e6f5297f6674f8a0254c
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-09-13 13:32:01 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("draw", (data) => {
+    // Broadcast the drawing data to all connected clients
+    console.log("drawing");
+    socket.broadcast.emit("draw", data);
+  });
+  socket.on("modify", (data) => {
+    console.log(`Object with id ${data.id} modified with data: `, data);
+
+    // broadcast the modify event to all other clients
+    socket.broadcast.emit("modify", data);
+  });
+  socket.on("userJoined", (data) => {
+    const { name, userId, roomId } = data;
+    socket.join(roomId);
+    socket.emit("userIsJoined", { success: true });
+  });
+});
+
+httpServer.listen(5000, () => {
+  console.log("Socket.IO server running on port 5000");
+});
